394b92b1a28c36739486d73a21167253802dcf45
Md Zafar <mz5659875@gmail.com>
2024-12-23 17:40:59 +0530

    Comment System Added

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const Blog = require('../models/blogModel');
+const Comment = require('../models/commentModel');
 
 const router = express.Router();
 
@@ -24,10 +25,21 @@ router.get('/add-new', (req, res)=>{
 
 router.get('/:id', async(req, res)=>{
     const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId:req.params.id}).populate("createdBy");
     return res.render('blog', {
         user:req.user,
         blog,
+        comments,
     });
+})
+router.post('/comment/:blogId', async(req, res)=>{
+    await Comment.create({
+        content:req.body.content,
+        blogId:req.params.blogId,
+        createdBy:req.user._id,
+    });
+
+    return res.redirect(`/blog/${req.params.blogId}`);  
 })
 
 router.post('/', upload.single('coverImg'), async(req, res)=>{
